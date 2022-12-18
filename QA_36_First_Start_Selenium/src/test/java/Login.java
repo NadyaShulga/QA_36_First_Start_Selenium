b8e818924c47fce3165a3d3dd2941f5df9936dd5
NadyaShulga <shulgaqa@gmail.com>
2022-12-18 19:46:46 +0200

    strategy of location
    the lesson #3

--- QA_36_First_Start_Selenium/src/test/java/Login.java
+++ QA_36_First_Start_Selenium/src/test/java/Login.java
@@ -1,9 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class Login {
     WebDriver wd;
@@ -14,23 +18,67 @@ public class Login {
         //open site
         wd.navigate().to("https://telranedu.web.app/");
     }
+
+    @Test
+    public void findElementsOnPage(){
+        //by tagName
+        WebElement element = wd.findElement(By.tagName("div"));
+        List<WebElement> divs = wd.findElements(By.tagName("div"));
+
+        //By id
+        WebElement el1 = wd.findElement(By.id("root"));
+        
+        //By class
+        WebElement el2 = wd.findElement(By.className("container"));
+
+        //By name
+        wd.findElement(By.name("name"));
+
+        //By link text a href
+        wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.partialLinkText("HO"));
+    }
     @Test
     public void loginSuccess(){
         //open form
-        //wd find button + click
+        //wd find item + click
+        WebElement itemLogin = wd.findElement(By.linkText("LOGIN"));
+        itemLogin.click(); //selenium does click button login
+
         //fill form
-        //find email + click + type
-        //find email + click + type
+        //find email + click + clear + type
+         WebElement email = wd.findElement(By.name("email"));
+         email.click();
+         email.clear();
+         email.sendKeys("shulgaqa@gmail.com");
+
+        //find email + click + clear + type
+        WebElement password = wd.findElement(By.name("password"));
+        password.click();
+        password.clear();
+        password.sendKeys("Sh$30091983");
+
         //submit form (click "login")
         //find button login + click
+        List<WebElement> buttons = wd.findElements(By.tagName("button"));
+        WebElement buttonLogin = buttons.get(0);
+        buttonLogin.click();
+
 
         Assert.assertEquals("", "");
         //ac == exp
     }
+
+    //homework
+    @Test
+    public void registrationSuccess(){
+
+    }
+
     @AfterMethod
     public void postCondition(){
         //close browser
-        wd.quit();
+       // wd.quit();
     }
 }
 
